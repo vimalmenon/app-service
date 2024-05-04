a3dbda40251aec5307abcb644b2234ea751fb4bc
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2024-05-04 11:51:04 +0800

    updated main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,13 @@
+"""
+This is main test
+"""
 from app.services.root_service import RootService
 
 
 def test_main():
+    """
+    This is main test
+    """
     result = RootService().data()
     assert result[0]["username"] == "Rick"
     assert result[1]["username"] == "Morty"
